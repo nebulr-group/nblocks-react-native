a32a3cd3ba262617658961b9336472093eb6d975
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-03 22:38:06 +0200

    Add ScrollView wrapper on login page

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -1,14 +1,19 @@
-import React, { FunctionComponent } from 'react';
-import {LoginComponent} from '../../components/Auth/LoginComponent/LoginComponent';
-import {DefaultPaddingComponent} from '../../components/shared/DefaultPaddingComponent';
-
+import React, { FunctionComponent } from "react";
+import { Platform, ScrollView } from "react-native";
+import { LoginComponent } from "../../components/Auth/LoginComponent/LoginComponent";
+import { DefaultPaddingComponent } from "../../components/shared/DefaultPaddingComponent";
 const LoginScreen: FunctionComponent<{}> = () => {
-
   return (
-    <DefaultPaddingComponent style={{flex: 1}}>
-      <LoginComponent />
-    </DefaultPaddingComponent>
+    <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardDismissMode={Platform.OS === "ios" ? "interactive" : "on-drag"}
+    >
+      <DefaultPaddingComponent style={{ flex: 1 }}>
+        <LoginComponent />
+      </DefaultPaddingComponent>
+    </ScrollView>
   );
-}
+};
 
-export {LoginScreen};
+export { LoginScreen };
