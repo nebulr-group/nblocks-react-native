ffede2774d90cdc0abaffa25e827e9769879a3fb
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-12 21:04:54 +0200

    Add ScrollView component to ResetPasswordScreen

--- src/screens/Auth/ResetPasswordScreen.tsx
+++ src/screens/Auth/ResetPasswordScreen.tsx
@@ -1,14 +1,20 @@
-import React, { FunctionComponent } from 'react';
-import {ResetPasswordComponent} from '../../components/Auth/ResetPasswordComponent/ResetPasswordComponent';
-import {DefaultPaddingComponent} from '../../components/shared/DefaultPaddingComponent';
+import React, { FunctionComponent } from "react";
+import { ScrollView, Platform } from "react-native";
+import { ResetPasswordComponent } from "../../components/Auth/ResetPasswordComponent/ResetPasswordComponent";
+import { DefaultPaddingComponent } from "../../components/shared/DefaultPaddingComponent";
 
 const ResetPasswordScreen: FunctionComponent<{}> = () => {
-
   return (
-    <DefaultPaddingComponent style={{flex: 1}}>
-      <ResetPasswordComponent />
-    </DefaultPaddingComponent>
+    <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardDismissMode={Platform.OS === "ios" ? "interactive" : "on-drag"}
+    >
+      <DefaultPaddingComponent style={{ flex: 1 }}>
+        <ResetPasswordComponent />
+      </DefaultPaddingComponent>
+    </ScrollView>
   );
-}
+};
 
-export {ResetPasswordScreen};
+export { ResetPasswordScreen };
