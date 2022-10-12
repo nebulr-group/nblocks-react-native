32be73f8675472afa222235a97544145b3ce81bb
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-12 23:19:26 +0200

    Create SetPasswordComponent for resetting user passwords

--- src/components/Auth/SetPasswordComponent/SetPasswordComponent.tsx
+++ src/components/Auth/SetPasswordComponent/SetPasswordComponent.tsx
@@ -1,33 +1,125 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import React, { FunctionComponent, useState } from "react";
-import { useApp } from "../../../hooks/app-context";
+import { Text, ActivityIndicator } from "react-native";
 import { useSecureContext } from "../../../hooks/secure-http-context";
 import { AuthRoutesStackParams } from "../../../routes/AuthRoutes";
-import {TextInputComponent} from "../../shared/InputComponent";
-import {NblocksButton} from "../../shared/NblocksButton";
-import {BaseStyleComponent} from "../BaseStyleComponent";
+import { TextInputComponent } from "../../shared/InputComponent";
+import { NblocksButton } from "../../shared/NblocksButton";
+import { BaseStyleComponent } from "../BaseStyleComponent";
+import { usePasswordValidation } from "../hooks/usePasswordValidation";
+import { ValidationMessageComponent } from "../ValidationMessageComponent/ValidationMessageComponent";
+import { useTheme } from "../../../hooks/theme-context";
+import * as Linking from "expo-linking";
+import QueryString from "qs";
+import { Fragment } from "react";
 
-const ResetPasswordComponent:FunctionComponent = () => {
+const SetPasswordComponent: FunctionComponent = () => {
+  const navigation = useNavigation<NavigationProp<AuthRoutesStackParams>>();
+  const [wasTouched, setWasTouched] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [requestErrorStatus, setRequestErrorStatus] = useState(false);
+  const { colors } = useTheme();
+  // Parse url for auth token
+  const url = Linking.useURL();
+  let token:
+    | string
+    | QueryString.ParsedQs
+    | string[]
+    | QueryString.ParsedQs[]
+    | undefined;
+  if (url) {
+    const parsedLink = Linking.parse(url);
+    if (parsedLink.queryParams) {
+      token = Linking.parse(url).queryParams!["token"];
+    }
+  }
 
-    const navigation = useNavigation<NavigationProp<AuthRoutesStackParams>>();
+  const { authService } = useSecureContext();
+  // Form Validity State/Control
+  const {
+    password: newPassword,
+    feedbackLog: newPasswordFeedbackLog,
+    passwordIsValid: newPasswordIsValid,
+    onPasswordTextChangeValidation: onNewPasswordTextChangeValidation,
+  } = usePasswordValidation();
 
-    const {authService} = useSecureContext();
+  const { password: confirmPassword, setPassword: setConfirmPassword } =
+    usePasswordValidation();
+  const submitPassword = async (): Promise<void> => {
+    try {
+      setLoading(true);
+      if (!token || typeof token !== "string") {
+        token = "";
+      }
+      await authService.updatePassword(token, newPassword);
+      navigation.navigate("Login");
+      setLoading(false);
+    } catch (error) {
+      setRequestErrorStatus(true);
+      setLoading(false);
+    }
+  };
 
-    const [password, setPassword] = useState("");
-
-    //TODO implement this view
-    return (
-        <BaseStyleComponent title="Set a new Password" subTitle="Please enter the email address used to sign in to your account.">
-            TODO!
-            {/* <TextInputComponent label="Email address" placeholder="Enter email address" type="emailAddress" value={username} onChangeText={(val) => setUsername(val)} />
-            <NblocksButton 
-                title="Get a reset link"
-                type="primary"
-                onPress={() => resetPassword()}
-                disabled={!username}
-            /> */}
+  let formIsValid = false;
+  if (newPasswordIsValid) {
+    formIsValid = samePasswordValidator(newPassword, confirmPassword);
+  }
+  const renderErrorMessages = wasTouched && !newPasswordIsValid;
+  return (
+    <Fragment>
+      {loading ? (
+        <ActivityIndicator color="#32B768" style={{ flex: 1 }} size="large" />
+      ) : (
+        <BaseStyleComponent
+          title="Set a new Password"
+          subTitle="Set your new password which you will be using for any future logins."
+        >
+          <TextInputComponent
+            label="New Password"
+            placeholder="Enter your new password."
+            type="password"
+            value={newPassword}
+            onChangeText={(text) => onNewPasswordTextChangeValidation(text)}
+            onPressIn={() => setWasTouched(true)}
+            onBlur={() => setWasTouched(true)}
+          />
+          {/* useEffect with clean up function should set the state of was touched */}
+          {renderErrorMessages && newPasswordFeedbackLog[0].error ? (
+            <ValidationMessageComponent feedbackLog={newPasswordFeedbackLog} />
+          ) : null}
+          <TextInputComponent
+            label="Confirm Password"
+            placeholder="Repeat your password"
+            type="password"
+            value={confirmPassword}
+            onChangeText={(text) => setConfirmPassword(text)}
+          />
+          <NblocksButton
+            title="Set New Password"
+            type={!requestErrorStatus ? "primary" : "danger"}
+            onPress={() => submitPassword()}
+            disabled={!formIsValid || requestErrorStatus}
+          />
+          {requestErrorStatus && (
+            <Text style={{ color: colors.dangerColor }}>
+              The link has expired, you'll need to generate a new one again.
+            </Text>
+          )}
         </BaseStyleComponent>
-    );
-}
+      )}
+    </Fragment>
+  );
+};
 
-export {ResetPasswordComponent};
+const samePasswordValidator = (
+  newPassword: string,
+  confirmPassword: string
+) => {
+  if (newPassword !== confirmPassword) {
+    return false;
+  }
+
+  return true;
+};
+
+export { SetPasswordComponent };
