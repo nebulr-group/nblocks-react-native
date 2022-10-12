589c662880233545855dfb24568780c32a7903c1
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-12 22:04:09 +0200

    Update SetPasswordScreen component context

--- src/screens/Auth/SetPasswordScreen.tsx
+++ src/screens/Auth/SetPasswordScreen.tsx
@@ -1,13 +1,19 @@
-import { Text } from "react-native";
 import { DefaultPaddingComponent } from "../../components/shared/DefaultPaddingComponent";
 import React, { FunctionComponent } from "react";
+import { Platform, ScrollView } from "react-native";
 import { SetPasswordComponent } from "../../components/Auth/SetPasswordComponent/SetPasswordComponent";
 
 const SetPasswordScreen: FunctionComponent = () => {
   return (
-    <DefaultPaddingComponent style={{ flex: 1 }}>
-      <SetPasswordComponent />
-    </DefaultPaddingComponent>
+    <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardDismissMode={Platform.OS === "ios" ? "interactive" : "on-drag"}
+    >
+      <DefaultPaddingComponent style={{ flex: 1 }}>
+        <SetPasswordComponent />
+      </DefaultPaddingComponent>
+    </ScrollView>
   );
 };
 
