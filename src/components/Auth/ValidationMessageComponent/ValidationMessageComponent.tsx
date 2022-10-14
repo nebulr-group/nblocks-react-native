878892a36574c735579f522e4811bb1f501635b2
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-14 11:59:04 +0200

    Add TSDoc write up for ValidationMessageObject interface & ValidationMessageComponent

--- src/components/Auth/ValidationMessageComponent/ValidationMessageComponent.tsx
+++ src/components/Auth/ValidationMessageComponent/ValidationMessageComponent.tsx
@@ -1,16 +1,41 @@
-import { Text } from "react-native";
+import { Text, TextStyle } from "react-native";
 import React, { Fragment, FunctionComponent } from "react";
 import uuid from "react-native-uuid";
 import { useTheme } from "../../../hooks/theme-context";
 
 // Password Validation Fedback Log Format
+/**
+ * Interface for constructing validation message objects.
+ *
+ * @remarks
+ * This interface is part of the ValidaitonMessageObject.
+ *
+ * @example
+ * Here's a simple example:
+ * ```
+ * // Object containing error: string map
+ * const messageObject: ValidationMessageObject = {
+ *  error: "Error message goes here."
+ * }
+ * ```
+ */
 export interface ValidationMessageObject {
+  /**
+   * The error message
+   */
   error?: string;
 }
 
+/**
+ * Validation message component for rendering list of messages.
+ * @param {ValidationMessageObject[]} feedbackLog - Array containing object map of key: string values.
+ * @param {TextStyle} style - Optional object containing style for messages, overrides the default style.
+ *
+ * @returns Fragment collection of Text elements that contain validation messages.
+ */
 const ValidationMessageComponent: FunctionComponent<{
   feedbackLog: ValidationMessageObject[];
-  style?: {};
+  style?: TextStyle;
 }> = ({ feedbackLog, style }) => {
   const { colors } = useTheme();
 
